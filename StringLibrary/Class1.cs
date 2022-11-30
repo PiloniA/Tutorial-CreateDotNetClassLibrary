d7739e32a54208ae3a72f7eb358d0733969b9a8c
PiloniA <piloni@live.at>
2022-11-30 22:16:55 +0100

    Write First Method Add Console App to Solution

--- StringLibrary/Class1.cs
+++ StringLibrary/Class1.cs
@@ -1,5 +1,13 @@
-﻿namespace StringLibrary;
-public class Class1
-{
+﻿namespace UtilityLibraries;
 
+public static class StringLibrary
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+        
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
 }
